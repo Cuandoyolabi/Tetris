689a8e0e1c87f0aa6e6602afe8d2d75b8ca645af
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-07-09 11:14:53 -0600

    "Creando Tetris, sin necesidad de la libreria P5"

---
+++ script.js
@@ -0,0 +1,14 @@
+let Pagina = {
+    isSetup: false,
+    body: document.getElementsByTagName('body')[0],
+    cvs: document.createElement('canvas'),
+    ctx: 0,
+    unitSize: 0,
+    AreaArr: [],
+
+    // Calculando el tamaño de la unidad, estructura y posicion delc canvas.
+    WindowChanged: function() {
+
+        //Calculando la
+    }
+}
